306a9de006a30b9e76424d4a1ff5502949b16285
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-05 16:57:00 -0500

    Update runner.java

--- 2024D3P1/src/runner.java
+++ 2024D3P1/src/runner.java
@@ -16,7 +16,8 @@ public class runner {
             while (test.contains(search)){
                 test = test.substring(test.indexOf(search));
                 String input = test.substring(test.indexOf(search), test.indexOf(")"));
-                if (Pattern.matches("\\d+,\\d+", input.substring(4))) {
+                input = input.substring(4)
+                if (Pattern.matches("\\d+,\\d+", input)) {
                     String[] list = input.split(",");
                     System.out.println(Arrays.toString(list));
                     sum += Integer.parseInt(list[0]) * Integer.parseInt(list[1]);
