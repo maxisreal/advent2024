ccf330b0a97979a20d65d0c260abf2d2fc795fa9
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-05 14:48:53 -0500

    dasdasdas

---
+++ 2024D3P1/src/runner.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class runner {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/input.txt");
+        int sum = 0;
+        for (String a : fileData){
+            String search = "mul(";
+            String test = a;
+            while (test.contains(search)){
+                String input;
+                test = test.substring(test.indexOf(search));
+                input = test.substring(test.indexOf(search), test.indexOf(")"));
+                input = input.substring(4);
+                String[] list = input.split(",");
+                System.out.println(Arrays.toString(list));
+                sum += Integer.parseInt(list[0]) * Integer.parseInt(list[1]);
+                test = test.substring(test.indexOf(search)+search.length());
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
