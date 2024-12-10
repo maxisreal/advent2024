fad6be54fd5347e38913a6a892008f704a5a0921
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-10 14:50:02 -0500

    dasdasdas

--- 2024D4P1/runner.java
+++ 2024D4P1/runner.java
@@ -7,83 +7,32 @@ import java.util.Scanner;
 public class runner {
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/input.txt");
+        ArrayList<String> fileData = getFileData("src/inputtest.txt");
+        System.out.println(fileData);
         int sum = 0;
         //forward & backward check
-        String[][] last = new String[fileData.size()][fileData.size()];
-        for (int a = 0; a<last.length; a++){
-            last[a] = fileData.get(a).split("");
-        }
-        for (String[] a : last){
-            String test = Arrays.toString(a);
-            //System.out.println(Arrays.toString(a));
-            //System.out.println(test);
+        for (int a = 0; a<fileData.size(); a++){
+            String test = fileData.get(a).toLowerCase();
             while (test.contains("xmas") || test.contains("samx")) {
                 sum++;
-                if (test.contains("xmas")) {
-                    test = test.substring(test.indexOf("xmas") + 3);
+                System.out.println(test);
+                if (test.contains("samx")) {
+                    test = test.substring(test.indexOf("samx") + 4);
                 }
-                if (test.contains("smax")) {
-                    test = test.substring(test.indexOf("smax") + 3);
-                }
-            }
-            System.out.println(test);
-        }
-        System.out.println("FIRST CHECK");
-        //up & down check
-        for (int a = 0; a<last.length; a++){
-            String test = "";
-            for (int i = 0; i<last.length; i++){
-                test += last[i][a];
-            }
-            System.out.println("FIRST CHECK");
-            while (test.contains("xmas") || test.contains("samx")) {
-                    sum++;
                 if (test.contains("xmas")) {
                     test = test.substring(test.indexOf("xmas") + 4);
                 }
-                if (test.contains("smax")) {
-                    test = test.substring(test.indexOf("smax") + 4);
-                }
                 System.out.println(test);
-                //test = test.substring(1);
             }
         }
+        System.out.println("FIRST CHECK");
+        System.out.println(sum);
+        //up & down check
+        
         System.out.println("SECOND CHECK");
+        System.out.println(sum);
         //diagonals
-        int num = 0;
-        for (int a = 0; a<140; a++){
-            String test = "";
-            for (int i = 0; i<last.length; i++){
-                test += last[num][i];
-                num++;
-            }
-            while (test.contains("xmas") || test.contains("samx")) {
-                sum++;
-                if (test.contains("xmas")) {
-                    test = test.substring(test.indexOf("xmas") + 3);
-                }
-                if (test.contains("smax")) {
-                    test = test.substring(test.indexOf("smax") + 3);
-                }
-            }
-            num -= num;
-            test = "";
-            for (int i = last.length; i>0; i--){
-                test += last[i-1-num][i];
-                num++;
-            }
-            while (test.contains("xmas") || test.contains("samx")) {
-                sum++;
-                if (test.contains("xmas")) {
-                    test = test.substring(test.indexOf("xmas") + 3);
-                }
-                if (test.contains("smax")) {
-                    test = test.substring(test.indexOf("smax") + 3);
-                }
-            }
-            num++;
-        }
+
         System.out.println("THIRD CHECK");
         //https://stackoverflow.com/questions/10713327/extracting-a-list-of-all-diagonals-from-a-matrix-in-a-specific-direction
         System.out.println(sum);
@@ -95,7 +44,7 @@ public class runner {
             File f = new File(fileName);
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
-                String line = s.nextLine().toLowerCase();
+                String line = s.nextLine();
                 if (!line.equals(""))
                     fileData.add(line);
             }
