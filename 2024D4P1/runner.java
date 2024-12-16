8a5ae73e80bef32c51b7b1c8f7c691d2fae2e976
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-16 12:21:13 -0500

    Update runner.java

--- 2024D4P1/runner.java
+++ 2024D4P1/runner.java
@@ -94,8 +94,9 @@ public class runner {
         }
         //TODO: ADD SOME SHIT HERE
         //https://www.geeksforgeeks.org/rotate-matrix-by-45-degrees/
-        System.out.println("THIRD CHECK");
+        //https://stackoverflow.com/questions/21212402/java-diagonal-checking
         //https://stackoverflow.com/questions/10713327/extracting-a-list-of-all-diagonals-from-a-matrix-in-a-specific-direction
+        System.out.println("THIRD CHECK");
         System.out.println(sum);
     }
 
