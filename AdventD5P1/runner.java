07ab95b3e19eb2e3556e89808f0589b23826902f
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-18 14:40:10 -0500

    Update runner.java

--- AdventD5P1/runner.java
+++ AdventD5P1/runner.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -9,7 +10,8 @@ public class runner {
 
         ArrayList<String> fileData = getFileData("src/input.txt");
         ArrayList<String[]> rules = new ArrayList<>();
-        ArrayList<String[]> pages = new ArrayList<>();
+        ArrayList<ArrayList<String>> pages = new ArrayList<>();
+        int sum = 0;
         for (int i = 0; i<fileData.size(); i++){
             if (Pattern.matches("\\d+\\|\\d+", fileData.get(i))){
                 rules.add(fileData.remove(i).split("\\|"));
@@ -17,12 +19,50 @@ public class runner {
             }
         }
         for (int i = 0; i<fileData.size(); i++){
-            pages.add(fileData.get(i).split(","));
+            String[] temp = fileData.get(i).split(",");
+            ArrayList<String> temp2 = new ArrayList<>();
+            for (String b : temp){
+                temp2.add(b);
+            }
+            pages.add(temp2);
         }
-        int sum = 0;
-        System.out.println(rules);
-        System.out.println(fileData);
-
+        //test printout
+        for (String[] a : rules){
+            for (String b : a){
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+        //test printout
+        for (ArrayList<String> a : pages){
+            System.out.println(a);
+            System.out.println(a.size());
+            System.out.println(a.size()/2);
+            System.out.println(a.get(a.size()/2));
+        }
+        for (int i = 0; i<pages.size(); i++){
+            boolean valid = true;
+            ArrayList<String> arr = pages.get(i);
+            for (int a = 0; a<rules.size(); a++){
+                String[] rule = rules.get(a);
+                if (arr.contains(rule[0]) && arr.contains(rule[1])) {
+                    if (arr.indexOf(rule[0]) > arr.indexOf(rule[1])) {
+                        valid = false;
+                    }
+                }
+            }
+            if (!valid){
+                pages.remove(i);
+                i--;
+            }
+        }
+        System.out.println("FILES REMOVED");
+        for (ArrayList<String> a : pages){
+            sum += Integer.parseInt(a.get(a.size()/2));
+            System.out.println(a);
+            System.out.println(a.get(a.size()/2));
+        }
+        System.out.println(sum);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
