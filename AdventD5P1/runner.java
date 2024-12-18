f7c8930a6bde02ba9f01656d80b599ba2d5a6bc9
maxisreal <145690663+maxisreal@users.noreply.github.com>
2024-12-18 12:19:13 -0500

    dasdasdas

---
+++ AdventD5P1/runner.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class runner {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/input.txt");
+        ArrayList<String> rules = new ArrayList<>();
+        for (int i = 0; i<fileData.size(); i++){
+            if (Pattern.matches("\\d+\\|\\d+", fileData.get(i))){
+                rules.add(fileData.remove(i));
+                i--;
+            }
+        }
+        int sum = 0;
+        System.out.println(rules);
+        System.out.println(fileData);
+
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
